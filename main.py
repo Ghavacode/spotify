fa674004766e929cb9de8ab569252e2ec6ef3e2a
Ghavacode <80360135+Ghavacode@users.noreply.github.com>
2021-03-13 14:30:33 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+git remote add origin https://github.com/Ghavacode/spotify.git
+git branch -M main
+git push -u origin main
+
+class CreatePlaylist:
+
+    def __init__(self):
+        pass
+
+    #step 1: log into youtube
+    def get_youtube_client(self):
+        pass
+    #step 2: Grab Our Liked Videos
+    def get_liked_videos(self):
+        pass
+    #Step 3: Create a new playlist
+    def create_playlist(self):
+        pass
+    #step 4: search for the song
+    def get_spotify_url(self):
+        pass
+    #step 5: Add the song into a new playlist
+    def ad_song_to_playlist(self):
+        pass
+
